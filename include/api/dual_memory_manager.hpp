586ccf08df51ac8d1665d2cd6b0e924a5ad88a62
TommasoTarchi <tommaso.tarchi@gmail.com>
2026-01-13 19:06:09 +0100

    total memory tracker

--- include/api/dual_memory_manager.hpp
+++ include/api/dual_memory_manager.hpp
@@ -3,6 +3,7 @@
 
 namespace DualMemoryManager {
 
+// TODO: add description and comments
 template <typename T> struct DualMemory {
   T *host_ptr;
   T *dev_ptr;
@@ -10,20 +11,27 @@ template <typename T> struct DualMemory {
   size_t size;
 };
 
+// TODO: add description
 // TODO: maye add alignment (either to the constructor or to the memory
 //       allocation call
 class DualMemoryManager {
+private:
+  size_t total_host_memory;  /*!< total used memory on host */
+  size_t total_device_memory;  /*!< total used memory on device */
+
 public:
-  DualMemoryManager(void) {}
+  DualMemoryManager(void) : total_host_memory(0), total_device_memory(0) {}
 
   template <typename T>
-  DualMemory<T> allocate(const size_t num_elements,
-                         const bool gpu_enabled = false) {
+  DualMemory<T> allocate(const std::string label, const size_t num_elements,
+                         const bool on_device = false) {
     DualMemory<T> dual_array;
 
+    /* allocate memory on host */
     dual_array.host_ptr = (T *)std::malloc(num_elements * sizeof(T));
 
-    if (gpu_enabled) {
+    /* if required, allocate memory on device */
+    if (on_device) {
       dual_array.dev_ptr = (T *)acc_malloc(num_elements * sizeof(T));
 
       if (dual_array.dev_ptr == nullptr) {
@@ -37,21 +45,31 @@ public:
       dual_array.dev_ptr = nullptr;
     }
 
+    /* update number of elements and bytes */
     dual_array.num_elements = num_elements;
     dual_array.size = num_elements * sizeof(T);
+
+    /* update used memory */
+    total_host_memory += dual_array.size;
+    if (on_device)
+      total_device_memory += dual_array.size;
 
     return dual_array;
   }
 
   template <typename T> void free(DualMemory<T> &dual_array) {
-    if (dual_array.dev_ptr != nullptr) {
+    /* free memory on host */
+    if (dual_array.host_ptr != nullptr) {
       std::free(dual_array.host_ptr);
       dual_array.host_ptr = nullptr;
+      total_host_memory -= dual_array.size;
     }
 
+    /* free memory on device (if present) */
     if (dual_array.dev_ptr != nullptr) {
       acc_free(dual_array.dev_ptr);
       dual_array.dev_ptr = nullptr;
+      total_device_memory -= dual_array.size;
     }
   }
 };
