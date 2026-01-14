b47b2e8e063aa6c0226081cd9a505a16fecc4877
TommasoTarchi <tommaso.tarchi@gmail.com>
2026-01-14 14:57:24 +0100

    formatting

--- tests/unit_tests_main.cpp
+++ tests/unit_tests_main.cpp
@@ -4,10 +4,7 @@
  * @brief Unit tests for the DualMemoryManager library, using Catch2.
  */
 
-
 #include "dual_memory_manager/dual_memory_manager.hpp"
 #include <catch2/catch_test_macros.hpp>
 
-TEST_CASE("general", "[dual_memory_manager]") {
-    REQUIRE(true);
-}
+TEST_CASE("general", "[dual_memory_manager]") { REQUIRE(true); }
